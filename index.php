8f57c46e53db9a85fe6f368f295eb7571bcc541e
Your Name <you@example.com>
2023-11-01 16:59:11 -0500

    c4

--- index.php
+++ index.php
@@ -13,36 +13,36 @@
     $dotenv = Dotenv::createImmutable(__DIR__);
     $dotenv->load();
   ?>
-  
+   <h1>Veterinaria AnimalSalud</h1>
   <div class="ContainerPrincipal" >
+    <div class="Iconos">
+      <div class="Icon">
+        <img src="./imagenes/perro.png" alt="">
+      </div>
+      <div class="Icon">
+        <img src="./imagenes/gato.png" alt="">
+      </div>
+      <div class="Icon">
+        <img src="./imagenes/cruz.png" alt="">
+      </div>
+    </div>
+    <div class="ImageDoctora">
+      <img src="./imagenes/Doctora.svg" alt="">
+    </div>
     <div class="RegistroContainer">
       <h2>Registro en la Clínica Veterinaria</h2>
       <form class="Registro" action="" method="post">
         <input type="text" name="nombre" placeholder="Nombre" required>
         <input type="text" name="username" placeholder="Nombre de usuario" required>
         <select name="Rol" id="Rol">
-          <option value="Usuario"></option>
-          <option value="Asministrador"></option>
+          <option value="Usuario">Usuario</option>
+          <option value="Administrador">Administrador</option>
         </select>
         <input type="email" name="email" placeholder="Correo Electrónico" required>
         <input type="password" name="password" placeholder="Contraseña" required>
         <button type="submit">Registrarse</button>
       </form>
     </div>  
-    <div class="slider-container">
-        <div class="slider">
-            <img src="./imagenes/conejo.jpg" alt="Imagen 1">
-            <img src="./imagenes/perro.jpg" alt="Imagen 2">
-            <img src="./imagenes/gato.jpg" alt="Imagen 3">
-            <img src="./imagenes/loro.jpg" alt="Imagen 4">
-        </div>
-        <div class="slider-indicator">
-            <span data-slide="1"></span>
-            <span data-slide="2"></span>
-            <span data-slide="3"></span>
-            <span data-slide="4"></span>
-        </div>
-    </div>
   </div>
   <script src="./js/main.js"></script>
 </body>
